ce2d4f37e2ecab2f404561dfdc308c81b2d3c8b6
Cheruiyot4008 <Cheruiyotbrian454@gmail.com>
2024-05-07 19:03:47 +0300

    corrected

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-""" Function that queries the Reddit API """
+"""function that queries the reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit."""
+
+
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """ Returns: top ten post titles
-        or None if queried subreddit is invalid """
-    headers = {'User-Agent': 'xica369'}
+    """function that queries the reddit API and prints the
+        titles of the first 10 hot posts listed for
+        a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'limit': 10}
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=parameters)
-
-    if response.status_code == 200:
-        titles_ = response.json().get('data').get('children')
-        for title_ in titles_:
-            print(title_.get('data').get('title'))
-    else:
-        print(None)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
